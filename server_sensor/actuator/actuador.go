e7f3004bfc569fbe1e2e016c525599a3bce04597
Marcus <88283829+PyMarcus@users.noreply.github.com>
2023-06-25 15:53:53 -0300

    Add files via upload

--- server_sensor/actuator/actuador.go
+++ server_sensor/actuator/actuador.go
@@ -1,1 +1,49 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+// atuador requisita informações de luminosidade a cada 10s
+type Actuator struct {
+	ServerIp   string
+	ServerPort string
+}
+
+// solicita, a cada 10s, informações de luminosidade ao servidor
+func (a Actuator) Request() {
+	const message string = "ASSINAR, LUMINOSIDADE"
+
+	buffer := make([]byte, 2048)
+
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", a.ServerIp, a.ServerPort))
+	defer conn.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("[+]Conectado ao servidor em: ", a.ServerIp, a.ServerPort)
+
+	for {
+		log.Println("[+]Esperando 10s")
+		time.Sleep(10 * time.Second)
+		_, err = conn.Write([]byte(message + "\n"))
+		log.Println("[+]Solicitando informacoes de luminosidade...")
+		if err != nil {
+			log.Panicln(err)
+		}
+		response, err := conn.Read(buffer)
+		if err != nil {
+			log.Println(err)
+		}
+		log.Println("[+]Resposta do servidor: ", string(response))
+	}
+}
+
+func main() {
+	actuador := Actuator{ServerIp: "127.0.0.1", ServerPort: "9999"}
+	actuador.Request()
+}
--- server_sensor/server/server.go
+++ server_sensor/server/server.go
@@ -1,1 +1,105 @@
 package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+// Armazena as informações enviadas pelo cliente sensor do tipo chave, valor
+var memory map[string]string = make(map[string]string)
+
+// Estrutura para representar o servidor, do tipo pública
+type Server struct {
+	Ip   string
+	Port string
+}
+
+// Definem o ip e a porta do servidor
+func (s *Server) setIpAndPort(ip, port string) {
+	s.Ip = ip
+	s.Port = port
+}
+
+// Cria a conexão tcp com os clientes, chamando a goroutine para tratá-los
+func (s Server) Service(ipAndPort ...string) {
+	if ipAndPort != nil {
+		s.setIpAndPort(ipAndPort[0], ipAndPort[1])
+	}
+	log.Printf("[+]Servidor em execucao em %s:%s \n", s.Ip, s.Port)
+
+	// socket em modo escuta
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%s", s.Ip, s.Port))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("[*]Aguardando conexoes...")
+
+	for {
+		// aceita conexoes com clientes
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("[-]Error %v\n", err)
+		}
+		log.Println("[+]Conexao recebida de: ", conn.RemoteAddr())
+		// inicia a goroutine para aceitar múltiplos clientes
+		go s.handleConnection(conn)
+	}
+
+}
+
+// Trata novas conexões
+func (s *Server) handleConnection(conn net.Conn) {
+	for {
+		// le os dados recebidos
+		buffer, err := bufio.NewReader(conn).ReadString('\n')
+
+		if err != nil {
+			log.Printf("[-]ERROR: %v\n", err)
+		}
+		// mensagem recebida e tratada
+		tbuffer := strings.TrimSpace(string(buffer))
+		toSend, err := s.parse(tbuffer)
+		if err != nil {
+			// diz que foi criado
+			log.Println(err)
+			_, err = conn.Write([]byte(toSend))
+		} else {
+			// salva retorna o valor solicitado
+			conn.Write([]byte(toSend))
+		}
+		log.Println("[+]Resposta '", toSend, "' enviada para: ", conn.RemoteAddr())
+	}
+}
+
+/*
+Trata a mensagem recebida.
+Se for do tipo publicar,
+salva no dicionario
+*/
+func (s Server) parse(message string) (toSend string, err error) {
+	commands := strings.Split(message, ",")
+	log.Println("[+]Tratando comando recebido: ", message)
+	// se o tamanho da mensagem for de 2 palavraas, indica uma requisicao do cliente atuador
+	if len(commands) == 2 {
+		if strings.TrimSpace(commands[0]) == "ASSINAR" {
+			return memory[strings.TrimSpace(commands[1])], nil
+		}
+		return "Falha! Comando inválido", errors.New("Comando inválido!")
+	}
+	// recebe mensagem do sensor para salvar nova informação
+	if strings.TrimSpace(commands[0]) == "PUBLICAR" {
+		memory[strings.TrimSpace(commands[1])] = strings.TrimSpace(commands[2])
+	}
+
+	return "created!", errors.New("Created!")
+}
+
+func main() {
+	server := Server{Ip: "127.0.0.1", Port: "9999"}
+	server.Service()
+}
